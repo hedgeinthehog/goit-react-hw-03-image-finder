e91cd8454da52a5e0352451d1ec7aa938defc7de
hedgeinthehog <37834796+hedgeinthehog@users.noreply.github.com>
2021-05-03 18:04:11 +0300

    adds gallery render by query

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,63 @@
 import React from 'react';
+import photosApi from '../../services/pixabay-api';
+import Searchbar from '../Searchbar';
+import ImageGallery from '../ImageGallery';
+import Button from '../Button';
 
-class Api extends React.Component {}
+class App extends React.Component {
+  state = {
+    photos: [],
+    searchQuery: '',
+    currentPage: 1,
+  };
 
-export default Api;
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery } = this.state;
+    if (searchQuery !== prevState.searchQuery) {
+      this.fetchPhotos();
+    }
+  }
+
+  handlQueryChange = query => {
+    this.setState({ photos: [], searchQuery: query, currentPage: 1 });
+  };
+
+  fetchPhotos = () => {
+    const { currentPage, searchQuery } = this.state;
+    const options = { query: searchQuery, page: currentPage };
+		
+    photosApi
+      .fetchPhotos(options)
+      .then(({ hits }) => {
+        console.log(hits);
+        this.setState(prevState => ({
+          photos: [...prevState.photos, ...hits],
+          currentPage: prevState.currentPage + 1,
+        }));
+      })
+      .then(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(console.log);
+  };
+
+  render() {
+    const { photos } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.handlQueryChange} />
+        <ImageGallery photos={photos} />
+        {/* <Loader />
+        
+        <Modal /> */}
+        {photos.length > 0 && <Button onClick={this.fetchPhotos} />}
+      </>
+    );
+  }
+}
+
+export default App;
--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '20681239-a14f4057e32edf906b8c27d2b';
 
-const fetchPhotos = (query, page, perPage = 12) => {
-  return axios.get(`${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}image_type=photo&orientation=horizontal&per_page=${perPage}`);
-},
+const Api = {
+  fetchPhotos({ query, page, perPage = 12 }) {
+    return axios
+      .get(
+        `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
+      )
+      .then(response => response.data);
+  },
+};
 
-export default {fetchPhotos};
+export default Api;
